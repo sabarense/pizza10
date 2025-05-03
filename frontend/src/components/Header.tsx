fa99d4ad04ad651408965b47ee9d51ab6ac571d2
Yan Sabarense <sabarensey@gmail.com>
2025-05-03 13:44:38 -0300

    [feat] header component

---
+++ frontend/src/components/Header.tsx
@@ -0,0 +1,19 @@
+import React from "react";
+import { Link } from "react-router-dom";
+import "../css/Header.css";
+
+const Header: React.FC = () => (
+  <header className="app-header">
+    <div className="app-header__logo">
+      <img src="/pizza-banner.jpg" alt="Logo Pizza10" />
+    </div>
+    <nav className="app-header__nav">
+      <Link to="/home">Home</Link>
+      <Link to="/pizza-config">Montar Pedido</Link>
+      <Link to="/drinks-config">Bebidas</Link>
+      <Link to="/register">Cadastro</Link>
+    </nav>
+  </header>
+);
+
+export default Header;
